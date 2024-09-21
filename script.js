732698692b2385089f449d255a9167a1d8fcd278
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 12:35:40 +0300

    created a submission

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ codes.forEach((code, idx) => {
 		if(keyPressed>=0 && keyPressed<=9){
 			codes[idx].value="";
 			if(codes.length-1 > idx){
+				codes[idx].blur;
 				setTimeout(()=> codes[idx+1].focus(),100);
 			}
 		}
 		else if(keyPressed === "Backspace"){
 			if(idx>0){
+				codes[idx].blur;
 				setTimeout(()=> codes[idx-1].focus(),100)
 			}
 		}
