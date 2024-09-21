c54fc294c1980c5b9dfb7b270af50f6d23a41749
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 12:19:47 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, idx) => {
+	code.addEventListener("keydown", (e)=>{
+		const keyPressed= e.key;
+
+		if(keyPressed>=0 && keyPressed<=9){
+			codes[idx].value="";
+			if(codes.length-1 > idx){
+				setTimeout(()=> codes[idx+1].focus(),100);
+			}
+		}
+		else if(keyPressed === "Backspace"){
+			if(idx>0){
+				setTimeout(()=> codes[idx-1].focus(),100)
+			}
+		}
+	})
+});
